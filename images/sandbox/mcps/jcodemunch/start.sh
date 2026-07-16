2cf8eb5c10a2a95f96eb648cefb24c2a6fd01896
Maxim Samoilov <git@nitive.me>
2026-07-16 13:42:57 +0200

    Add jcodemunch config

--- images/sandbox/mcps/jcodemunch/start.sh
+++ images/sandbox/mcps/jcodemunch/start.sh
@@ -4,4 +4,7 @@ root_dir="$(dirname "${BASH_SOURCE[0]}")"
 
 export UV_PROJECT_ENVIRONMENT=.venv
 
+mkdir -p ~/.code-index/
+cat /opt/mcps/jcodemunch/config.jsonc > ~/.code-index/config.jsonc
+
 "$root_dir/.venv/bin/jcodemunch-mcp"
